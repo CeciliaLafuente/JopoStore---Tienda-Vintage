3592c8e2674568e75b0d7c0e03079e4f2a72e2c7
Leonel1973 <leomariani_1@hotmail.com>
2022-07-09 12:08:26 -0300

    Se agregan modificaciones

--- Grupo4/app.js
+++ Grupo4/app.js
@@ -6,7 +6,7 @@ let app=express();
 const publicPath= path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.listen(3040, ()=>{
+app.listen(process.env.PORT || 3040, ()=>{
     console.log("Servidor corriendo en el puerto 3040");
 })
 
