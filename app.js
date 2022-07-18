263dc6d0d4abf4b5d8a1201047f53e664f2f8d65
Leonel1973 <leomariani_1@hotmail.com>
2022-07-18 11:24:56 -0300

    Se agregan rutas y controladores login y registro

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ app.use (express.static(publicPath));
 
 // const mainRouter = require ('./src/routers/main.js');
 const productsRouter = require ('./src/routers/products.js');
-// const usersRouter = require ('./src/routers/users.js');
+const usersRouter = require ('./src/routers/users.js');
 
 
 // app.use ('/', mainRouter);
 app.use ('/products', productsRouter);
-// app.use ('/users', usersRouter);
+app.use ('/users', usersRouter);
 
 app.set ("view engine", "ejs");
 app.set ("views", path.join (__dirname, './src/views'));
+
 
 
 app.listen(process.env.PORT || 3040, ()=>{
---
+++ src/controllers/usersController.js
@@ -0,0 +1,11 @@
+const usersController={
+    login: function (req,res){
+        res.render('./users/login');  
+    },
+
+    register: function(req, res){
+        res.render('./users/register');
+    }
+}
+
+module.exports=usersController;
