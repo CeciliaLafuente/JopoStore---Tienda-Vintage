ed58449aaa6ab92ad7c2cff3d22c508174f1143a
ceciliaLafuente <lafuentececilia@hotmail.com>
2022-07-20 17:01:48 -0300

    controlador y router de index

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 let app = express();
 
-const publicPath= path.join(__dirname,'./public');
-app.use (express.static(publicPath));
+const publicPath = path.join(__dirname, "./public");
+app.use(express.static(publicPath));
 
-// const mainRouter = require ('./src/routers/main.js');
-const productsRouter = require ('./src/routers/products.js');
-const usersRouter = require ('./src/routers/users.js');
+const indexRouter = require("./src/routers/index.js");
+const productsRouter = require("./src/routers/products.js");
+const usersRouter = require("./src/routers/users.js");
 
+app.use("/", indexRouter);
+app.use("/products", productsRouter);
+app.use("/users", usersRouter);
 
-// app.use ('/', mainRouter);
-app.use ('/products', productsRouter);
-app.use ('/users', usersRouter);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./src/views"));
 
-app.set ("view engine", "ejs");
-app.set ("views", path.join (__dirname, './src/views'));
-
-
-
-app.listen(process.env.PORT || 3040, ()=>{
-    console.log("Servidor corriendo en el puerto 3040");
-})
-
-
-let home= path.join(__dirname,'./src/views/index.html');
-let login= path.join(__dirname,'./src/views/users/login.html');
-let registro= path.join(__dirname,'./src/views/users/register.html');
-
-
-app.get('/', function(req,res){
-    res.render('index');
-})
-
-app.get('/registro', function(req,res){
-    res.sendFile(registro);
-})
-
-app.get('/login', function(req,res){
-    res.sendFile(login);
-})
-
+app.listen(process.env.PORT || 3040, () => {
+console.log("Servidor corriendo en el puerto 3040");
+});
 
 
 
