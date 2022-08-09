1a577688c87b1e2626c0edb3137667344187ac1d
ceciliaLafuente <lafuentececilia@hotmail.com>
2022-08-08 21:23:23 -0300

    correcciones

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -7,32 +7,19 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
 
 const controller = {
 
     shoppingCart: (req, res) => {
-<<<<<<< HEAD
         res.render ('products/shoppingCart');
     },
-=======
-        res.render ('./products/shoppingCart');
-    },
-    productDetail: (req, res) => {
-        let product = products.find ( valor => {
-            return valor.id == req.params.id;
-        });
-
-        res.render ('./products/productDetail', {product, toThousand});
-    }
->>>>>>> 4685b506e887dc007699bc91971f7fc33472b180
 
     productDetail: (req, res) => {
-    let product = products.filter(valor => {
+    let product = products.find(valor => {
         return valor.id == req.params.id;
     });
-    res.render ('./products/productDetail', {product});
+    console.log(product)
+    res.render ('./products/productDetail', {product, toThousand});
     },
 
     productsList: (req, res) => {
--- src/routers/products.js
+++ src/routers/products.js
@@ -11,9 +11,6 @@ router.get ('/productDetail/:id', productsController.productDetail);
 
 router.get ('/productsList', productsController.productsList);
 
-<<<<<<< HEAD
 router.post ('/productsList/filtro', productsController.filtroPorCategoria);
 
-=======
->>>>>>> 4685b506e887dc007699bc91971f7fc33472b180
 module.exports = router;
