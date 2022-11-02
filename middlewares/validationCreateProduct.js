22ba35ea215c46b5dae813b9fe753c26e4f7a4d3
marcelabus <guidoymarcela@hotmail.com>
2022-11-02 17:11:37 -0300

    emparejé valid de producto BE y FE

--- middlewares/validationCreateProduct.js
+++ middlewares/validationCreateProduct.js
@@ -3,9 +3,9 @@ const { body }= require('express-validator');
 const validation = [
     
     body('name').notEmpty().withMessage('Debes completar nombre del producto').bail()
-    .isLength({min:3, max:20}).withMessage('Debe tener mínimo 3 caracteres y máximo 20'),
+    .isLength({min:5}).withMessage('Debe tener mínimo 5 caracteres'),
     body('description').notEmpty().withMessage('Debes agregar una descripción').bail()
-    .isLength({max:100}).withMessage('Máximo 100 caracteres'),
+    .isLength({min:20,max:300}).withMessage('Debe tener entre 20 y 300 caracteres'),
     body('category_id').notEmpty().withMessage('Debes seleccionar una categoría').bail(),
     body('colors').notEmpty().withMessage('Debes seleccionar al menos un color').bail(),
     body('price').isNumeric().notEmpty().withMessage('Debes colocar el precio').bail()
